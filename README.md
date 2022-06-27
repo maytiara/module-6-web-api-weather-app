db7e51b689d271f5c53ba00b9adef60f095de7a7
May Carandang <maytiara@gmail.com>
2022-06-27 14:20:09 +0800

    all files added for module 6

---
+++ README.md
@@ -0,0 +1,32 @@
+# Module 06: Weather Forecast [Using Web-server API]
+
+## Project Objective
+
+⋅⋅⋅ Utilise the data coming from an open-source API (https://openweathermap.org/api/one-call-api). During the process {as a "Rookie Developer"}, I notice there were multiple options you use to fetch data using the API link, for e.g. "by calling different parameter from the open-source API, or use the other links available to fetch the current/5daily forecast.
+
+## The roadmap to get the results required for this Module 
+#### * Provide the Search form with 5 days forecast interface {done}
+#### * Enter diff cities using search and get the weather results {incomplete}
+#### * Presented with current location and time {done}
+#### * Presented with current temp, wind speed, humidity and UV Index (optional) {incomplete}
+#### * Searched city and saved in the local storage using JSON.parse {incomplete}
+
+## User Story [Ref: Module 6 required]:
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+## Mock-Up
+
+![Mobile interface of weather app - INCOMPLETE](./assets/weather-forecast-incomplete.gif)
+
+
+## Deployed link:
+Incomplete [link text itself].
+
+
+- - -
+Code References: Boostrap, customize css styles, third party and with mixed of tutor coding ideas.
---
+++ index.js
@@ -0,0 +1,64 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const appKey ='0cc20eb061ce1f620419a27e3dc314a7';
+
+//Function for Current time location
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
+    dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+
+}, 1000);
+
+//function for current forecast
+let weather = {
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+};
+
+//add an event listener to the form
+document.getElementById("submit-btn").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    getElementById("search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+//incomplete
