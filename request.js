3f3c05dfba279dec14c7ceefe4e35ec59c7fc555
May Carandang <maytiara@gmail.com>
2022-06-29 13:31:35 +0800

    Changed filename app.js,added UV Index & variable

---
+++ request.js
@@ -0,0 +1,4 @@
+//API CALL for this Module
+//Source: https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&unit=metric&appid={API key}
+
+const weatherKey = '0cc20eb061ce1f620419a27e3dc314a7';
