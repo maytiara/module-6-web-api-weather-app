257f2e4e6b0929a19de03f66d1812bf60ad8ed49
May Carandang <maytiara@gmail.com>
2022-06-30 15:57:31 +0800

    changed search form class name

--- app.js
+++ app.js
@@ -1,66 +1,103 @@
+//Display for the current time
+
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
-const currentWeatherItemsEl = document.getElementById('current-weather-items');
-const timezone = document.getElementById('time-zone');
-const countryEl = document.getElementById('country');
-const weatherForecastEl = document.getElementById('weather-forecast');
-const currentTempEl = document.getElementById('current-temp');
 
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-const appKey ='0cc20eb061ce1f620419a27e3dc314a7';
 
 //Function for Current time location
 setInterval(() => {
-    const time = new Date();
-    const month = time.getMonth();
-    const date = time.getDate();
-    const day = time.getDay();
-    const hour = time.getHours();
-    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
-    const minutes = time.getMinutes();
-    const ampm = hour >=12 ? 'PM' : 'AM'
+    let time = new Date();
+    let month = time.getMonth();
+    let date = time.getDate();
+    let day = time.getDay();
+    let hour = time.getHours();
+    let hoursIn12HrFormat = hour >= 13 ? hour %12: hour
+    let minutes = time.getMinutes();
+    let ampm = hour >=12 ? 'PM' : 'AM'
 
     timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
     dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
 
 }, 1000);
 
-//function for current forecast
-//let weather = {
-//    fetchWeather: function (city) {
-//      fetch(
-//        "https://api.openweathermap.org/data/2.5/weather?q=" +
-//          city +
-//          "&units=metric&appid=" +
-//          this.apiKey
-//      )
-//        .then((response) => {
-//          if (!response.ok) {
-//            alert("No weather found.");
-//            throw new Error("No weather found.");
-//          }
-//          return response.json();
-//        })
-//        .then((data) => this.displayWeather(data));
-//    },
-//};
-//
-////add an event listener to the form
-//document.getElementById("submit-btn").addEventListener("click", function () {
-//    weather.search();
-//  });
-//  
-//  document
-//    getElementById("search-bar")
-//    .addEventListener("keyup", function (event) {
-//      if (event.key == "Enter") {
-//        weather.search();
-//    }
-//});
-
-//incomplete
-
 // API CALL 
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+//const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        
+        let {latitude, longitude } = success.coords;
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${weatherKey}`).then(res => res.json()).then(data => {
+
+        console.log(data)
+        showWeatherData(data);
+        })
+
+    })
+}
+
+function showWeatherData (data){
+    let {humidity, temp, wind_speed, uvi} = data.current;
+
+    timezone.innerHTML = data.timezone;
+    //countryEl.innerHTML = data.lat + 'N ' + data.lon+'E'
+
+    currentWeatherItemsEl.innerHTML = 
+    `
+    <article class="weather-item">
+        <section>Temperature:</section>
+        <section id="temp">${temp} &#176;C</section>
+    </article>
+    <article class="weather-item">
+        <section>Wind Speed:</section>
+        <section id="wind">${wind_speed} mph</section>
+    </article>
+    <article class="weather-item">
+        <section>Humidity:</section>
+        <section id="humidity">${humidity} %</section>
+    </article>
+    <article class="weather-item">
+        <section>UV index:</section>
+        <section id="uv-index">${uvi}</section>
+    </article>  
+    `;
+
+    let otherDayForecast = ''
+    data.daily.forEach((day, idx) => {
+        if(idx == 0){
+            currentTempEl.innerHTML = 
+            `<section class="future-forecast">
+                <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <h3 class="day">${window.moment(day.dt*1000).format('dddd')}</h3>
+                <h4 class="temp">Night : ${day.temp.night}&#176; C</h4>
+                <h4 class="temp">Day : ${day.temp.day}&#176; C</h4>
+                <h4 class="wind">Wind : ${data.current.wind_speed}mph</h4>
+                <h4 class="humidity">Humidity : ${data.current.humidity}%</h4>
+            </section>
+            `
+        }else{
+            otherDayForecast += 
+            `
+                <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <h3 class="day">${window.moment(day.dt*1000).format('dddd')}</h3>
+                <h4 class="temp">Night : ${day.temp.night}&#176; C</h4>
+                <h4 class="temp">Day : ${day.temp.day}&#176; C</h4>
+                <h4 class="wind">Wind : ${data.current.wind_speed}mph</h4>
+                <h4 class="humidity">Humidity : ${data.current.humidity}%</h4>
+            `
+        }
+    })
+
+
+    weatherForecastEl.innerHTML = otherDayForecast;
+}
--- request.js
+++ request.js
@@ -2,3 +2,14 @@
 //Source: https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&unit=metric&appid={API key}
 
 const weatherKey = '0cc20eb061ce1f620419a27e3dc314a7';
+const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=-31.95224&lon=115.8614&unit=metric&appid=0dd5c652307a249518a0bb1731562de8';
+
+//Fetch data from the given url
+//fetch(currentWeatherUrl).
+//    then((data) => { 
+//        console.log ('response',data.json()) })
+//
+//    //catching the error, in case the call rejected
+//    .catch((error) => {
+//        console.log(error);
+//    });
