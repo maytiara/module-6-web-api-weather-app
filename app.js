d161a6c75cbf98dd6fda076c6007d2a182e9fade
May Carandang <maytiara@gmail.com>
2022-07-01 22:40:17 +0800

    Add function fetch: cities, timezone, weathers

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Display for the current time
+//Display for the current time and current search city
 
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
@@ -6,10 +6,11 @@ const dateEl = document.getElementById('date');
 let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-
-//Function for Current time location
-setInterval(() => {
+function setTimeWithTimeZone(timezone) {
     let time = new Date();
+    if (timezone) {
+        time = new Date(time.toLocaleString('en-US', {timeZone: timezone}))
+    }
     let month = time.getMonth();
     let date = time.getDate();
     let day = time.getDay();
@@ -21,16 +22,16 @@ setInterval(() => {
     timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
     dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
 
-}, 1000);
+}
 
-// API CALL 
+//Function for Current time location => setTimeWithTimeZone(timezone)
+let timer=setInterval(setTimeWithTimeZone, 1000);
+
+// Assigned Variables for API CALL 
 const currentWeatherItemsEl = document.getElementById('current-weather-items');
 const timezone = document.getElementById('time-zone');
-//const countryEl = document.getElementById('country');
-const weatherForecastEl = document.getElementById('weather-forecast');
-const currentTempEl = document.getElementById('current-temp');
 
-
+// Function for an API CALL to get the humidity, temp,, wind speed, Uvi
 getWeatherData()
 function getWeatherData () {
     navigator.geolocation.getCurrentPosition((success) => {
@@ -49,9 +50,14 @@ function getWeatherData () {
 function showWeatherData (data){
     let {humidity, temp, wind_speed, uvi} = data.current;
 
+    //Placeholder for => function setTimeWithTimeZone(timezone)
     timezone.innerHTML = data.timezone;
-    //countryEl.innerHTML = data.lat + 'N ' + data.lon+'E'
 
+    //Getting the current city data => timezone
+    clearInterval(timer)
+    timer=setInterval(() => setTimeWithTimeZone(data.timezone), 1000);
+
+    //placeholder for => showWeatherData from the API
     currentWeatherItemsEl.innerHTML = 
     `
     <article class="weather-item">
@@ -70,34 +76,91 @@ function showWeatherData (data){
         <section>UV index:</section>
         <section id="uv-index">${uvi}</section>
     </article>  
-    `;
-
-    let otherDayForecast = ''
-    data.daily.forEach((day, idx) => {
-        if(idx == 0){
-            currentTempEl.innerHTML = 
-            `<section class="future-forecast">
-                <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <h3 class="day">${window.moment(day.dt*1000).format('dddd')}</h3>
-                <h4 class="temp">Night : ${day.temp.night}&#176; C</h4>
-                <h4 class="temp">Day : ${day.temp.day}&#176; C</h4>
-                <h4 class="wind">Wind : ${data.current.wind_speed}mph</h4>
-                <h4 class="humidity">Humidity : ${data.current.humidity}%</h4>
-            </section>
-            `
-        }else{
-            otherDayForecast += 
-            `
-                <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
-                <h3 class="day">${window.moment(day.dt*1000).format('dddd')}</h3>
-                <h4 class="temp">Night : ${day.temp.night}&#176; C</h4>
-                <h4 class="temp">Day : ${day.temp.day}&#176; C</h4>
-                <h4 class="wind">Wind : ${data.current.wind_speed}mph</h4>
-                <h4 class="humidity">Humidity : ${data.current.humidity}%</h4>
-            `
-        }
-    })
-
-
-    weatherForecastEl.innerHTML = otherDayForecast;
+    `
 }
+
+
+//---LOCAL STORAGE-----Will change later
+function getCitiesFromStorage() {
+    if (localStorage.getItem("cities"))
+        return JSON.parse(localStorage.getItem("cities"));
+    return [];
+}
+
+//--Fetch the cities data from other API CALL
+const app = {
+    init: () => {
+      document
+        .getElementById('submit-btn')
+        .addEventListener('click', app.fetchCoordinates);
+
+        //get Item from local storage ---Will change later
+        let cities = getCitiesFromStorage()
+        // loop over cities and show them on html ---Will change later
+    
+    },
+    fetchCoordinates: (ev) => {
+        ev.preventDefault();
+        var cityName = document.getElementById("search-bar").value;
+        let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${weatherKey}`
+        
+        //----In addition, to add local storage ---Will change later
+        let cities = getCitiesFromStorage()
+        cities.push(cityName)
+        localStorage.setItem("cities", JSON.stringify(cities));
+        
+        fetch(url)
+        .then((resp) => {
+          if (!resp.ok) throw new Error(resp.statusText);
+          return resp.json();
+        })
+        .then((data) => {
+          app.fetchWeather(data[0]);
+        })
+        .catch(console.err);
+    },
+    fetchWeather: (data) => {
+      //use the values from latitude and longitude to fetch the weather
+      let lat = data.lat;
+      let lon = data.lon;
+      let lang = 'en';
+      let units = 'metric';
+      let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherKey}&units=${units}&lang=${lang}`;
+
+      //fetch the weather
+      fetch(url)
+        .then((resp) => {
+          if (!resp.ok) throw new Error(resp.statusText);
+          return resp.json();
+        })
+        .then((data) => {
+            showWeatherData(data)
+            app.showDailyWeather(data);
+        })
+        .catch(console.err);
+    },
+    
+    showDailyWeather: (resp) => {
+      console.log(resp);
+      let row = document.getElementById('weather-forecast');
+      //clear out the old weather and add the new
+      // row.innerHTML = '';
+      row.innerHTML = resp.daily
+        .map((day, idx) => {
+          if (idx <= 4) {
+            let dt = new Date(day.dt * 1000); //timestamp * 1000
+            return `<section class="weather-forecast-item">
+                <h3 class="day">${dt.toDateString()}</h3>
+                <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                <h4 class="temp">High ${day.temp.max}&deg;C Low ${day.temp.min}&deg;C</h4>
+                <h4 class="wind">Wind: ${day.wind_speed}m/s</h4>
+                <h4 class="humidity">Humidity : ${day.humidity}%</h4>
+            </section>`
+          }
+        })
+        .join(' ');
+    },
+  };
+  
+  app.init();
+
