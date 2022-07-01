d40579ace015ac812e30d1908d5a09aebe8cd2f5
May Carandang <maytiara@gmail.com>
2022-07-01 22:21:24 +0800

    Add comments

--- request.js
+++ request.js
@@ -1,5 +1,7 @@
 //API CALL for this Module
 //Source: https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&unit=metric&appid={API key}
+//Source to get location: http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={API key}
+//Source to get icon: https://openweathermap.org/weather-conditions
 
 const weatherKey = '0cc20eb061ce1f620419a27e3dc314a7';
 const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=-31.95224&lon=115.8614&unit=metric&appid=0dd5c652307a249518a0bb1731562de8';
@@ -9,7 +11,7 @@ const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=-
 //    then((data) => { 
 //        console.log ('response',data.json()) })
 //
-//    //catching the error, in case the call rejected
+//catching the error, in case the call rejected
 //    .catch((error) => {
 //        console.log(error);
 //    });
